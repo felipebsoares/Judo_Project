b86a2dd94e08b3ae3bb34edc98ba35abeb3dffbe
felipebsoares <103378305+felipebsoares@users.noreply.github.com>
2022-11-26 19:33:59 -0300

    Added scoped and automapper profile

--- backend/ProjetoJudo/ProjetoJudo.Api/Program.cs
+++ backend/ProjetoJudo/ProjetoJudo.Api/Program.cs
@@ -1,3 +1,9 @@
+using ProjetoJudo.Application.Contracts.Services;
+using ProjetoJudo.Application.Notification;
+using ProjetoJudo.Application.Services;
+using ProjetoJudo.Domain.Contracts.Repositories;
+using ProjetoJudo.Infra.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services
+    .AddScoped<IUsuarioRepository, TbUsuarioRepository>()
+    .AddScoped<IAuthService, AuthService>()
+    .AddScoped<INotificator, Notificator>();
 
 var app = builder.Build();
 
