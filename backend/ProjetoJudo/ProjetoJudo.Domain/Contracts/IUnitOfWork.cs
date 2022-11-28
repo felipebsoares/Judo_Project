8c48863b6cd4105849678afc0b3f740634b3e00c
NatanLima62 <111523011+BackEndNDS@users.noreply.github.com>
2022-11-27 23:16:24 -0300

    Finalizacao do repository

--- backend/ProjetoJudo/ProjetoJudo.Domain/Contracts/IUnitOfWork.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ProjetoJudo.Domain.Contracts;
-
-public interface IUnitOfWork
-{
-    Task<bool> Commit();
-}
---
+++ backend/ProjetoJudo/ProjetoJudo.Infra/Repositories/AtletaRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoJudo.Domain.Contracts.Repositories;
+using ProjetoJudo.Domain.Entities;
+using ProjetoJudo.Infra.Abstractions;
+using ProjetoJudo.Infra.Context;
+
+namespace ProjetoJudo.Infra.Repositories;
+
+public class TbAtletaRepository : Repository<TbAtleta>, IAtletaRepository
+{
+    public TbAtletaRepository(JudoDesContext context) : base(context)
+    {
+    }
+
+    public void Create(TbAtleta tbAtleta)
+    {
+        Context.TbAtletas.Add(tbAtleta);
+    }
+
+    public void Update(TbAtleta tbAtleta)
+    {
+        Context.TbAtletas.Update(tbAtleta);
+    }
+
+    public async Task<TbAtleta?> ObterPorId(int id)
+    {
+        return await FistOrDefault(at => at.IdAtleta == id);
+    }
+
+    public async Task<List<TbAtleta>?> ObterTodos()
+    {
+        var allUsers = await Context.Set<TbAtleta>()
+            .AsNoTracking()
+            .ToListAsync();
+
+        return allUsers;
+    }
+
+    public void Remover(TbAtleta tbAtleta)
+    {
+        Context.TbAtletas.Remove(tbAtleta);
+    }
+}
