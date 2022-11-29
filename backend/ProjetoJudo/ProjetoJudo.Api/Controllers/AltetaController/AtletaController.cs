b6d5095dfcebdb2282acf9de619121e036537dd6
felipebsoares <103378305+felipebsoares@users.noreply.github.com>
2022-11-29 15:25:57 -0300

    TRYINg

--- backend/ProjetoJudo/ProjetoJudo.Api/Controllers/AltetaController/AtletaController.cs
+++ backend/ProjetoJudo/ProjetoJudo.Api/Controllers/AltetaController/AtletaController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
 using ProjetoJudo.Application.Contracts;
 using ProjetoJudo.Application.DTOs.Atleta;
 using ProjetoJudo.Application.DTOs.Usuarios;
@@ -13,24 +14,42 @@ namespace ProjetoJudo.Api.Controllers.AltetaController
     {
         private readonly IAtletaService _atletaService;
 
+        public static class lista
+        {
+            private static List<CreateAtletaDto> atletas = new();
+        }
+
+
         public AtletaController(IAtletaService atletaService)
         {
             _atletaService = atletaService;
+            
         }
         
         [HttpPost("Registrar-Atleta")]
-        [Authorize]
-        public async Task<IActionResult> Create([FromBody] CreateAtletaDto dto)
+        public Task<IActionResult> Create([FromBody] CreateAtletaDto dto)
         {
-            var atleta = await _atletaService.Create(dto);
-            return Ok(atleta);
+            atletas.Add(dto);
+            Atletas.Add(dto);
+            return Ok(dto);
         }
         
         [HttpPut("Atualizar-Atleta-{id}")]
-        [Authorize]
         public async Task<IActionResult> Update(int id, [FromBody] UpdateAtletaDto dto)
         {
             var atleta = await _atletaService.Update(id, dto);
+            return Ok(atleta);
+        }
+
+        [HttpGet("{cpf}")]
+        public async Task<IActionResult> GetByCpf(string cpf)
+        {
+            var atleta = Atletas.FirstOrDefault(c => c.Cpf == cpf);
+            if (atleta == null)
+            {
+                return null;
+            }
+
             return Ok(atleta);
         }
     }
--- backend/ProjetoJudo/ProjetoJudo.Application/DTOs/Atleta/CreateAtletaDto.cs
+++ backend/ProjetoJudo/ProjetoJudo.Application/DTOs/Atleta/CreateAtletaDto.cs
@@ -1,36 +1,24 @@
-﻿namespace ProjetoJudo.Application.DTOs.Atleta;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoJudo.Application.DTOs.Atleta;
 
 public class CreateAtletaDto
 {
-    public string RegistroFederacao { get; set; } = null!;
-    public string? RegistroConfederacao { get; set; }
+    public CreateAtletaDto(int idAtleta, string nome, DateTime dataNascimento, string email, string cpf, List<CreateAtletaDto> atletas)
+    {
+        IdAtleta = idAtleta;
+        Nome = nome;
+        DataNascimento = dataNascimento;
+        Email = email;
+        Cpf = cpf;
+        Atletas = atletas;
+    }
+
+    public int IdAtleta { get; set; }
     public string Nome { get; set; } = null!;
     public DateTime DataNascimento { get; set; }
-    public DateTime DataFiliacao { get; set; }
-    public int IdAgremiacao { get; set; }
-    public string Cep { get; set; } = null!;
-    public string Endereco { get; set; } = null!;
-    public string? Bairro { get; set; }
-    public string? Complemento { get; set; }
-    public int IdCidade { get; set; }
-    public int IdEstado { get; set; }
-    public int IdPais { get; set; }
-    public string Telefone { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string Cpf { get; set; } = null!;
-    public string Identidade { get; set; } = null!;
-    public DateTime DataIdentidade { get; set; }
-    public int IdEmissor { get; set; }
-    public int IdNacionalidade { get; set; }
-    public int IdProfissaoAtleta { get; set; }
-    public string? NomePai { get; set; }
-    public int? IdProfissaoPai { get; set; }
-    public string? NomeMae { get; set; }
-    public int? IdProfissaoMae { get; set; }
-    public int IdFaixa { get; set; }
-    public int IdSexo { get; set; }
-    public int IdEstadoCivil { get; set; }
-    public string? Anotacoes { get; set; }
-    public bool Selecao { get; set; }
-    public int IdCliente { get; set; }
+
+    public static List<CreateAtletaDto> Atletas { get; set; } = new();
 }
